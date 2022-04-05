515f350227bfcb5b69f786b59ce450b2ed8fefd1
hayhaydz <haydon1900@gmail.com>
2022-04-05 17:42:24 +0100

    fixed untracked files

--- utils/CalculateData.js
+++ utils/CalculateData.js
@@ -3,6 +3,10 @@ export const calculateAQI = (aqi) => {
   return 100 - ((aqi / 500) * 100);
 }
 
+export const calculatePopulationDensity = (pop, area) => {
+  return Math.round((pop / (area / 1000000)) * 100) / 100;
+}
+
 export const calculateRating = ({ aqi }) => {
   let AQIpercentage = calculateAQI(aqi);
 
